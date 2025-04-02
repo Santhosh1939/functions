b6b01ee5c5c33fecac049ff0a92e03e23fc7e5d8
SANTHOSH KUMAR GOWDA <gowda.santhu1939@gmail.com>
2025-04-01 20:38:46 -0400

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,98 @@
+# Task 1: Add Function
+# Write a Python function named add that takes two arguments a and b and
+# returns their sum.
+# def add():
+#     numb= 5
+#     numb_1=6
+#     result=numb+numb_1
+#     print(result)
+# add()
+# or
+# def add(n1,n2):
+#     return(n1+n2)
+# print(add(5,6))
+
+
+# Task 2: Square Function
+# Write a Python function named square that takes a number x as input and
+# returns its square.
+# def square():
+#     x= int(input("enter a number :"))
+#     result=x*x
+#     print(result)
+# square()
+# or
+# def square(a):
+#      return(a*a)
+# print(square(8))
+
+# Task 3: Factorial Function
+# Write a Python function named factorial that takes a positive integer n as
+# input and returns its factorial.
+
+# def factorial(n):
+#     factorial = 1  
+#     for i in range(1, n + 1):
+#         factorial *= i  
+#     return factorial  
+# n = int(input("Enter an integer: "))
+# print("Factorial of", n, "is", factorial(n))
+
+# Task 4: Maximum Function
+# Write a Python function named maximum that takes a list of numbers as input and
+# returns the maximum value in the list.
+# maximum=(1,25,45,12,15,96,78,21,35,45,21)
+# def maxmimum_1():
+#     maximum=(1,25,45,12,15,96,78,21,35,45,21)
+#     result= max(maximum)
+#     print(result)
+# maxmimum_1()
+
+# Task 5: Reverse Function
+# Write a Python function named reverse that takes a string s as input and
+# returns its reverse.
+# def reverse():
+#     s=("my name is santhosh")
+#     revers=s[::-1]
+#     print(revers)
+# reverse()
+
+
+# Task 6: Check Prime Function
+# Write a Python function named is_prime that takes a positive integer n as input
+# and returns True if n is prime, otherwise False .
+# num= int
+# def is_prime(n):
+#     num < 1
+#     if num<=1:
+#         return False
+#     if num == num%1==1 and num%num==1:
+#         return True
+#     if num == num%1!=1 and num%num!=1:
+#         return False
+# is_prime()
+    
+# Task 7: Fibonacci Function
+# Write a Python function named fibonacci that takes a positive integer n as
+# input and returns the n th Fibonacci number.
+def fibonacci(n):
+    if n >1:
+        n==n-1+n-2
+    return fibonacci
+
+n = int(input("Enter an integer: "))
+print(fibonacci)
+
+# Task 8: Palindrome Function
+# Write a Python function named is_palindrome that takes a string s as input and
+# returns True if s is a palindrome, otherwise False .
+
+
+# Task 9: Sum of Squares Function
+# Write a Python function named sum_of_squares that takes a list of numbers as
+# input and returns the sum of the squares of those numbers.
+
+
+# Task 10: Average Function
+# Write a Python function named average that takes a list of numbers as input and
+# returns the average value.
